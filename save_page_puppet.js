5534d0b073f26854f1f22d35730f5be0d2983d99
Etienne Rebetez <etienne.rebetez@gmail.com>
2018-05-17 12:23:05 +0200

    WIP

--- save_page_puppet.js
+++ save_page_puppet.js
@@ -1,29 +1,41 @@
 'use strict';
 
+const fs = require('fs');
 const puppeteer = require('puppeteer');
+const xmlserializer = require('xmlserializer');
+
+var html2xhtml = function (htmlString) {
+    var parser = require('parse5'),
+        dom = parser.parse(htmlString);
+ 
+    return xmlserializer.serializeToString(dom);
+};
 
 (async() => {
 
 const browser = await puppeteer.launch({
-    executablePath: '/bin/chromium',
-    headless: false
+    executablePath: '/bin/chromium'
+    ,headless: false
    });
 const page = await browser.newPage();
 
 await page.goto('http://www.xcontest.org/switzerland/de/fluge/tageswertung-pg/#filter[date]=2018-05-12', {waitUntil: 'networkidle0'}); //networkidle0 domcontentloaded
 
 
-//let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-console.log("HEY!!!!")
+console.log("Loaded!")
 
-//new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML;
 
 //const flights = await page.$('#flights');
+
+//let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+
 await page.screenshot({path: 'example.png'});
 
+var xhtml = html2xhtml(document.body.innerHTML);
 
+await fs.write('1.html', xhtml, 'w');
 //page.$(selector)
 
-//await browser.close();
+await browser.close();
 
 })(); 

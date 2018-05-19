c9a6bcad62d1a789edb2b0f993d3acd429bedf4c
Etienne Rebetez <etienne.rebetez@gmail.com>
2018-05-19 11:17:36 +0200

    basic meteoblue csv parsing

--- parsing_weather.py
+++ parsing_weather.py
@@ -1,6 +1,15 @@
 import csv
 
-with open('VQHA69.csv') as csvfile: #, newline=''
-     weather = csv.reader(csvfile, delimiter='|')
+daily_wather = list()
+
+with open('weather/biel_2018-05-17T21_47_38.csv') as csvfile: #, newline=''
+     weather = csv.reader(csvfile, delimiter=';')
      for row in weather:
-         print(', '.join(row)) 
+         if row and row[3] == '12': ## NOTE only get data at 12 o'clock.
+           daily_wather.append(
+               {
+                   row[0] + '-' + row[1] + '-' + row[2]: row[5:-1]
+               })
+           #print(', '.join(row)) 
+
+print(daily_wather)

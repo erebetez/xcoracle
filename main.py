2db128a87e85204e0c82721bf61a509ecc54cac9
Etienne Rebetez <etienne.rebetez@gmail.com>
2018-05-19 10:45:34 +0200

    make a label list of locations

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+from datetime import date, timedelta
 import parsing_flights as pf
 
-print(pf.parseFlights('cleaned_flights.xml'))
+data_folder = 'flights/data/'
+
+
+
+def best_locaction_per_day(day):
+    locationRankingList = pf.parseFlights(data_folder + day + '.xml')
+
+    #print(locationRankingList)
+
+    best_loc = pf.getBestLocation(locationRankingList)
+
+    return best_loc
+
+#print(best_locaction_per_day('2018-05-06'))
+
+
+d1 = date(2018, 5, 3)  # start date
+d2 = date(2018, 5, 16)  # end date
+
+delta = d2 - d1         # timedelta
+
+label_list = list()
+
+for i in range(delta.days + 1):
+    date = d1 + timedelta(days=i)
+    label_list.append({date.isoformat(): best_locaction_per_day(date.isoformat())})
+
+print(label_list)
--- parsing_flights.py
+++ parsing_flights.py
@@ -14,35 +14,45 @@ def parseFlights(fileName):
     locationRankingList = []
 
     for flight in root.findall('flight'):
-        loc = flight.find('location').text
-        currentList = locationDict.get(loc, list())
-        points = [float(s) for s in flight.find('points').text.split() if is_number(s)]
-        print(points)
-        currentList.append(
-            {
-                #'count': (len(currentList) + 1),
-                'time': flight.find('time').text,
-                'points': points[0],
-                'length': flight.find('length').text
-            })
-        locationDict[loc] = currentList
+        if flight.find('country').text == "Switzerland":
+            loc = flight.find('location').text
+            currentList = locationDict.get(loc, list())
+            points = [float(s) for s in flight.find('points').text.split() if is_number(s)]
+            currentList.append(
+                {
+                    'time': flight.find('time').text,
+                    'points': points[0],
+                    'length': flight.find('length').text
+                })
+            locationDict[loc] = currentList
 
     for location, flightList in locationDict.items():
         count = len(flightList)
         pointList = map(lambda d: d.get('points'), flightList)
         average = sum(pointList) / count
+        #print(pointList)
+        #max_point = max(pointList)
         locationRankingList.append(
             {
                 'location': location,
                 'count': count,
-            #   'max': max(pointList),
+                #'max': max_point,
                 'average': average,
                 'flights': flightList
             }
-            )
+        )
 
-
-    locationRankingList = sorted(locationRankingList, key=lambda loc: loc.get('average'), reverse=True)
+    locationRankingList = sorted(locationRankingList, key=lambda loc: loc.get('average'))
 
     return locationRankingList
-    
+
+def getBestLocation(locationRankingList):
+    min_flights = 3
+    min_points = 50
+
+    filtered = [loc for loc in locationRankingList if (loc['count'] >= min_flights and
+                                                       loc['average'] >= min_points)]
+    if not filtered:
+        return '' # for sake of consitency
+    else:
+        return filtered.pop()['location']
